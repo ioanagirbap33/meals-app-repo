4e76e9af380d89787a12e53a501c2c45ccdcb047
ioana.girba <>
2024-04-01 15:22:22 +0300

    Output meal categories

---
+++ meals-app/components/CategoryGridTitle.js
@@ -0,0 +1,13 @@
+import { Pressable, View, Text } from "react-native";
+
+export const CategoryGridTitle = ({ title, color }) => {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
