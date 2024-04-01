a7b31f822d1481a89501d6d6191ffdf60803820c
ioana.girba <>
2024-04-01 19:17:04 +0300

    Add background color and status bar

--- meals-app/App.js
+++ meals-app/App.js
@@ -1,15 +1,17 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, StatusBar, View } from "react-native";
 import { CategoriesScreen } from "./screens/CategoriesScreen";
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <View style={styles.wrapper}>
+      <StatusBar styles="light" />
+      <CategoriesScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  wrapper: {
+    backgroundColor: "#24180f",
   },
 });
