81f428001237666525edcba7f97bf0026e118bf2
ioana.girba <>
2024-04-02 13:33:13 +0300

    Style meal details screen

--- meals-app/components/MealItem.js
+++ meals-app/components/MealItem.js
@@ -1,5 +1,6 @@
 import { View, Text, Pressable, StyleSheet, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { MealDetails } from "./MealDetails";
 export const MealItem = ({
   id,
   title,
@@ -27,11 +28,11 @@ export const MealItem = ({
             <Image style={styles.image} source={{ uri: imageUrl }} />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}m</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+          />
         </View>
       </Pressable>
     </View>
@@ -64,15 +65,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fonstSize: 18,
     margin: 8,
-  },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8,
-    justifyContent: "center",
-  },
-  detailItem: {
-    marginHorizontal: 8,
-    fontSize: 12,
   },
 });
--- meals-app/screens/MealDetailsScreen.js
+++ meals-app/screens/MealDetailsScreen.js
@@ -1,11 +1,87 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+import { MealDetails } from "../components/MealDetails";
 
 export const MealDetailsScreen = ({ route }) => {
-  const catId = route.params.mealId;
+  const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View>
-      <Text style={{ color: "white" }}>Meal details: {catId}</Text>
-    </View>
+    <ScrollView style={styles.listContainer}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listContainer}>
+        <View>
+          <View style={styles.subtitleContainer}>
+            <Text style={styles.subtitle}>Ingredients</Text>
+          </View>
+          {selectedMeal.ingredients.map((ingredient) => (
+            <Text style={styles.listItem} key={ingredient}>
+              {ingredient}
+            </Text>
+          ))}
+
+          <View style={styles.subtitleContainer}>
+            <Text style={styles.subtitle}>Steps</Text>
+          </View>
+
+          {selectedMeal.steps.map((step) => (
+            <Text style={styles.listItem} key={step}>
+              {step}
+            </Text>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  subtitle: {
+    color: "#c0a28c",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subtitleContainer: {
+    margin: 6,
+    borderBottomColor: "#c0a28c",
+    borderBottomWidth: 2,
+    marginHorizontal: 24,
+    marginVertical: 4,
+  },
+
+  listItem: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginVertical: 4,
+    marginHorizontal: 12,
+    backgroundColor: "#c0a28c",
+    borderRadius: 6,
+  },
+  listContainer: {
+    marginBottom: 32,
+  },
+});
